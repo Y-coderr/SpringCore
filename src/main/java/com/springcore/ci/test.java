1f5a774f86a346ced7dd5ca26d338ec7403f3d3e
Yash Kadam <kadamyash6112@gmail.com>
2025-01-09 10:13:49 +0530

    Showing an error message

--- src/main/java/com/springcore/ci/test.java
+++ src/main/java/com/springcore/ci/test.java
@@ -1,10 +1,12 @@
 package com.springcore.ci;
-import org.springframework.context.*;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class test {
     public static void main(String[] args) {
-        ApplicationContext context =new ClassPathXmlApplicationContext("com/springcore/ci/ciconfig.xml");
+        ApplicationContext context =new ClassPathXmlApplicationContext("file:ciconfig.xml");
         Addition add=(Addition)context.getBean("add");
         add.doSum();
     }
