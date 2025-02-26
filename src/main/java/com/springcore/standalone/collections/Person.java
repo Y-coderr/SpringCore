99604a1bc0293042d943bf80c3b3f7b808af6786
Yash Kadam <kadamyash6112@gmail.com>
2025-02-26 10:48:20 +0530

    stand alone collection

--- src/main/java/com/springcore/standalone/collections/Person.java
+++ src/main/java/com/springcore/standalone/collections/Person.java
@@ -1,4 +1,22 @@
 package com.springcore.standalone.collections;
 
+import java.util.List;
+
 public class Person {
+    private List<String> friends;
+
+    public List<String> getFriends() {
+        return friends;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "friends=" + friends +
+                '}';
+    }
+
+    public void setFriends(List<String> friends) {
+        this.friends = friends;
+    }
 }
--- src/main/java/com/springcore/standalone/collections/Test.java
+++ src/main/java/com/springcore/standalone/collections/Test.java
@@ -1,4 +1,13 @@
 package com.springcore.standalone.collections;
 
+import org.springframework.context.ApplicationContext;
+import com.springcore.standalone.collections.Person;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
+    public static void main(String[] args) {
+        ApplicationContext context =new ClassPathXmlApplicationContext("aloneconfig.xml");
+        Person person1 = (Person) context.getBean("person1",Person.class);
+        System.out.println(person1);
+    }
 }
