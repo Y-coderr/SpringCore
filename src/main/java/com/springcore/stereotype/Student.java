f38821e27fdd186892ab4e95191cd155bc9a43cc
Yash Kadam <kadamyash6112@gmail.com>
2025-03-26 09:36:12 +0530

    steretypes annotation notes

--- src/main/java/com/springcore/stereotype/Student.java
+++ src/main/java/com/springcore/stereotype/Student.java
@@ -1,11 +1,13 @@
 package com.springcore.stereotype;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component("ob")
+@Component("ob")   // for defining the object of the bean
+@Scope("prototype")   // for declaring scope
 public class Student {
     @Value("Yash Kadam")
     private String studentName;
--- src/main/java/com/springcore/stereotype/Test.java
+++ src/main/java/com/springcore/stereotype/Test.java
@@ -7,6 +7,13 @@ public class Test {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("stereoconfig.xml");
         Student student = context.getBean("ob", Student.class);
-        System.out.println(student);
+        System.out.println(student.hashCode());
     }
 }
+
+
+// notes
+//Bean Scope
+// there are some types of scope
+//1.singleton 2.prototype 3.request 4.session 5.GlobalSession
+//1.Singleton (ByDefault):- in this scope the object of student return the same bean object repeatetly for the confirmation we havr to check hashcode of this bean object
